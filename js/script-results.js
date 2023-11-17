cae87f7724eb5af605d1bffa6e3262536a0e550a
VczEno <arpaia.vin@gmail.com>
2023-11-17 17:49:12 +0100

    results update HTML, js

--- js/script-results.js
+++ js/script-results.js
@@ -1,4 +1,18 @@
+let correctAnswersCounter= 4
+
 function examResult () {
+    let correctScore= document.querySelectorAll('.left-box p')
+    console.log(correctScore)
+    correctScore[1].innerText=(correctAnswersCounter/10)*100 + '%'
+    correctScore[2].innerText=correctAnswersCounter +'/10 questions'
+    let wrongScore= document.querySelectorAll('.right-box p')
+    console.log(wrongScore)
+    wrongScore[1].innerText=((10-correctAnswersCounter)/10)*100 + '%'
+    wrongScore[2].innerText=(10-correctAnswersCounter) +'/10 questions'
+    let pieChart=document.querySelector('#pieChart')
+    console.log(pieChart.innerHTML)
+    pieChart.style=`--p:${(correctAnswersCounter/10)*100}`
+   
     let textArea= document.querySelector('.circle-text')
     let p1= document.createElement('p')
     let p2= document.createElement('p')
@@ -8,8 +22,7 @@ function examResult () {
     textArea.appendChild(p1)
     textArea.appendChild(p2)
     textArea.appendChild(p3)
-    let correctAnswers= 75
-    if(correctAnswers >= 60) {
+    if((correctAnswersCounter/10)*100 >= 60) {
         let passedSound=document.querySelector('#passed')
         setTimeout(startConfetti, 2000)
         setTimeout(passedSound.play(), 2000)
@@ -19,13 +32,19 @@ function examResult () {
         p3.innerText='We\'ll send you the certificate in few minutes. Check your email (including promotions / spam folder)'
         
     } else {
-        p1.innerText='Bad news Fella'
-        p2.innerText='You didn\' pass the exam.'
+        let failedSound=document.querySelector('#failed')
+        setTimeout(failedSound.play(), 2000)
+        p1.innerText='Whoops...Try again'
+        p2.innerText='You didn\'t pass the exam.'
         p2.className= 'pink-text'
-        p3.innerText='You should get your ass out the class, you\'re embarassing bro...you make your mama cry'
+        p3.innerText="Since you're not a machine like me, you're build up to fail. Don't worry, take your time to focus on your mistakes and the next time you will succeed"
     }
 }
 
 examResult()
+
+
+
+
 
 
